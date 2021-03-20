bae545e4f9c921c79f0992207dd5008b49684079
rereRaghad <78446332+rereRaghad@users.noreply.github.com>
2021-03-20 20:37:35 +0300

    first update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./components/header"
+import {
+    MDBNavbar,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBContainer,
+    MDBNavbarToggler,
+    MDBIcon } from 'mdb-react-ui-kit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+    <Header />
+
+    <div
+      className='p-5 text-center bg-image'
+      style={{ backgroundImage: "url('https://i.pinimg.com/originals/89/23/a2/8923a220c509f3e1a69af46b3588434e.jpg')", height: 600 }}
+    >
+      <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}>
+        <div className='d-flex justify-content-center align-items-center h-100'>
+          <div className='text-white'>
+            <h1 className='mb-3'>Easy Print</h1>
+            <h4 className='mb-3'> الطباعة السهلة</h4>
+            <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
+              ابدأ الآن
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <p className='mt-4'>Scroll down</p>
+    <p>Scroll down</p>
+    <p>Scroll down</p>
+    <p>Scroll down</p>
+    <p>Scroll down</p>
+    <p>Scroll down</p>
+    <p>Scroll down</p>
+    <p>Scroll down</p>
+  </header>
+  
   );
 }
 
