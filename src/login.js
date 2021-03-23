f3c52724dfb4f0ffbba9c622867a327133679908
rereRaghad <78446332+rereRaghad@users.noreply.github.com>
2021-03-23 20:00:01 +0300

    .

--- src/login.js
+++ src/login.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './register.css'
-function login() {
+import { Link, useHistory } from "react-router-dom";
+import { useStateValue } from "./components/stateProvidor";
+
+function Login() {
+
+  const history = useHistory();
+  // const [{ }, dispatch] = useStateValue("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
+
+
+  // useEffect(() => {
+        
+  //   if (user == null || user == ""  ) {
+  //       dispatch({
+  //           type: 'SET_USER',
+  //           user: null
+  //       })
+  //   } else {
+  //       console.log(user);
+  //       history.push("/");
+  //   }
+  // }, [user]) //will only run when then app component loads
+  
+  const Login = async () => {
+    const requestOptions = {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" },
+      body: JSON.stringify({ Email: 'Ahmed@gmail.com', PasswordHash:"raghad221" })
+  };
+  fetch('https://localhost:44399/api/PrintingShopLogin', requestOptions)
+    
+  }
+
     return (
         <div>
         <div className="header">تسجيل دخول مقدم الخدمة</div>
@@ -9,24 +44,26 @@ function login() {
           </div>
           <div className="form">
             <div className="form-group">
-              <label htmlFor="CoName"> الاسم التجاري</label>
-              <input type="text" name="CoName" />
+              <label htmlFor="CoName">: الأيميل </label>
+              <input type="text" name="Email" 
+               onChange={(e) => setEmail(e.target.value)}/>
             </div>
            
             <div className="form-group">
-              <label htmlFor="password"> كلمة السر </label>
+              <label htmlFor="password">: كلمة السر </label>
               <input type="password" name="password"
-                onChange = {(event,newValue) => this.setState({password:newValue})}  />
+                 onChange={(e) => setPassword(e.target.value)}/>
             </div>
         
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          
+          <button type="button" className="btn" onClick={Login}>
              تسجيل الدخول
           </button>
         </div>
       </div>   
     )
     }
-export default login
+export default Login
