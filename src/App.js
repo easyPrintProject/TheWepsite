cc8c9bbc94fc0b10b39c3c707acb632762ba31c5
rawan <63139645+rawan321@users.noreply.github.com>
2021-03-23 13:57:29 +0300

    add progress line to register to address

--- src/App.js
+++ src/App.js
@@ -8,23 +8,26 @@ import Pricing from "./pricing"
 import Login from "./login"
 import Register from "./register"
 import  Features from "./features"
-
-
-
-
+import  IsCourses from "./isCourses"
+import Dashboard from "./dashboard"
+import Address from "./address"
+import Products from "./products"
 
 function App() {
   return (
     <Router>
      <Header/>
      <Switch>
-
-
         <Route path="/Login">  <Login/>  </Route>
         <Route path="/Register">  <Register/>  </Route>
         <Route path="/Pricing">  <Pricing />   </Route>
         <Route path="/About">  <About />    </Route>
         <Route path="/Features">  <Features />    </Route>
+        <Route path="/isCourses">  <IsCourses />    </Route>
+        <Route path="/address">  <Address />    </Route>
+        <Route path="/products">  <Products />    </Route>
+        <Route path="/dashboard">  <Dashboard />    </Route>
+
         <Route path="/">  <Home />   </Route>
          
 
