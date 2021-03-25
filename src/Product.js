c82cd2f4da07820e9015ac1f384728b426fc643a
rawan <63139645+rawan321@users.noreply.github.com>
2021-03-26 02:13:07 +0300

    update

---
+++ src/Product.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import { Button, Modal } from 'react-bootstrap';
+import { IconContext } from 'react-icons';
+import * as FaIcons from 'react-icons/fa';
+import * as FiFeather from "react-icons/fi"
+import {
+  MDBIcon } from 'mdb-react-ui-kit';
+
+
+function Product() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Product
