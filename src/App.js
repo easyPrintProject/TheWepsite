64fc206ef06e7081d013a406b9bc21f1a9d6715c
fatmahamer <Fatmahamer22@gmail.com.com>
2021-03-26 18:25:34 +0300

    6

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/header"
+import Footer from "./components/Footer"
 import Home from "./home"
-import About from "./about"
+import About from "./About"
 import Pricing from "./pricing"
 import Login from "./login"
 import Register from "./register"
@@ -12,14 +13,18 @@ import PrintingShopInfo from "./PrintingShopInfo"
 import AdressInfo from "./AdressInfo"
 import AddDrivers from "./AddDrivers"
 import PrintShopProfile from "./PrintShopProfile";
-import Addcoupons from "./Addcoupons"
-
-
+import Cuponspage from "./Cuponspage"
+import Branchprinter from "./Branchprinter"
 
 function App() {
   return (
+    <div className="page-container">
+      <div className="content-wrap">
+
+     
     <Router>
      <Header/>
+   
      <Switch>
 
 
@@ -32,13 +37,17 @@ function App() {
         <Route path="/AdressInfo">  <AdressInfo />    </Route>
         <Route path="/PrintShopProfile">  <PrintShopProfile />    </Route>
         <Route path="/AddDrivers">  <AddDrivers />   </Route>
-        <Route path="/Addcoupons">  <Addcoupons />   </Route>
+        <Route path="/Cuponspage">  <Cuponspage />   </Route>
+        <Route path="/Branchprinter">  <Branchprinter />   </Route>
 
-
+        Branchprinter
     </Switch>
+    {/* <Footer/> */}
 </Router>
  
+</div>
 
+    </div>
    
   
   );
---
+++ src/Cuponspage.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Modal } from 'react-bootstrap';
+import {MDBIcon } from 'mdb-react-ui-kit';
+
+
+ function Cuponspage() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    const [fullName, setFullName] = React.useState("");
+  const [city, setCity ] = React.useState("");
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] =  React.useState("");
+  const [phoneNumber, setPhoneNumber] =  React.useState("");
+  const [commercialName, setCommercialName] =  React.useState("");
+  const [user, setUser] =  React.useState({Email:"", UserName:"", PhoneNumber:"",  EmailConf:false, errorMassage:"", Id:"", Token:""});
+  const [errorMassage, setErrorMassage] =  React.useState("");
+  
+  return (  <div className="form">
+
+  
+    <h1> اضافة كوبون جديد</h1>
+  
+    <div style={{ backgroundImage: "url(/tag.png)" ,width:500,height:500}}>
+
+</div>
+    <input type="text" name="Name" style={{marginTop:50,width:87,height:50}} />
+ 
+
+    <h1 htmlFor="Phone" > رقم جوال العميل</h1>
+    <input type="text" name="Phone"  />
+
+
+    <h1 htmlFor="Email" >البريد الإلكتروني</h1>
+    <input type="email" name="Eamil" />
+  
+  
+
+<div className="footer">
+      <button type="button" className="btn" onClick={handleShow}>
+        أرسل
+      </button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>! تهانينا </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>تمت أضافة الكوبون الى(اسم الشخص)</Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={handleClose}>
+            تم
+          </Button>
+        </Modal.Footer>
+      </Modal>
+         </div>
+         </div>
+  );
+}
+export default Cuponspage
